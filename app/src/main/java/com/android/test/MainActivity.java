a6d6a997e5287e2fca19e2a4b41a635089af3b5a
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-08-09 11:08:36 +0500

    view binding enabled

--- app/src/main/java/com/android/test/MainActivity.java
+++ app/src/main/java/com/android/test/MainActivity.java
@@ -8,14 +8,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.test.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    // Declare a variable to hold the generated binding class for activity_main.xml
+    // This allows direct access to all views in the layout via View Binding
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
